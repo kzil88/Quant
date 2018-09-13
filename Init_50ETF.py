e0064fbd3619e000c6500857ec5121f6514ebc58
Mingxin WANG <37770806+kzil88@users.noreply.github.com>
2018-09-13 10:26:07 +0800

    Update Init_50ETF.py

--- Init_50ETF.py
+++ Init_50ETF.py
@@ -13,8 +13,6 @@ if __name__ == '__main__':
     time_temp = datetime.datetime.now() - datetime.timedelta(days=1)
     end_dt = time_temp.strftime('%Y-%m-%d')
 
-    #temp_index = ts.get_hist_data(code= 'sh',start='2015-03-10',end=end_dt)
-
     temp_index = ts.bar(code='510050', conn = cons,freq='D',end_date=end_dt,ma=[5,10,20,30,60])
     print(temp_index)
 
