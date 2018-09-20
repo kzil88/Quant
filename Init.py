5ef8c495fd163f78d233f6bc272daa38f9ea9656
Mingxin WANG <37770806+kzil88@users.noreply.github.com>
2018-09-20 08:59:48 +0800

    Update Init.py

--- Init.py
+++ Init.py
@@ -12,7 +12,6 @@ if __name__ == '__main__':
     start_dt = '2016-01-01'
     time_temp = datetime.datetime.now() - datetime.timedelta(days=1)
     end_dt = time_temp.strftime('%Y-%m-%d')
-
     # 取股票代码清单
     src = ts.get_stock_basics()
     code_list = src.index
