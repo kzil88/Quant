949d97158044c90a7b4bfb149c8dfdc0f5feee1a
Mingxin WANG <37770806+kzil88@users.noreply.github.com>
2018-10-07 16:50:41 +0800

    Update Init_Forecast.py

--- Init_Forecast.py
+++ Init_Forecast.py
@@ -9,6 +9,7 @@ if __name__ == '__main__':
     # 建立数据库连接,剔除已入库的部分
     db = pymysql.connect(host='127.0.0.1', user='root', passwd='admin', db='stock', charset='utf8')
     cursor = db.cursor()
+    
     year = 2017
     for i in range(4):
         temp_day = ts.forecast_data(year,i+1)
