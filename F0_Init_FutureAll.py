eadce5a2544cdc7db7a8e1d0a4f00f79a264d0ca
Mingxin WANG <37770806+kzil88@users.noreply.github.com>
2018-10-09 08:42:40 +0800

    Update F0_Init_FutureAll.py

--- F0_Init_FutureAll.py
+++ F0_Init_FutureAll.py
@@ -5,6 +5,7 @@ import pymysql
 import re
 
 
+
 if __name__ == '__main__':
     end_dt = (datetime.datetime.now() - datetime.timedelta(days=1)).strftime('%Y-%m-%d')
     db = pymysql.connect(host='127.0.0.1', user='root', passwd='admin', db='future', charset='utf8')
