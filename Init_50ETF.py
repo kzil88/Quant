ff7b4b395b433f07cb571b1e04b63ff54f396042
Mingxin WANG <37770806+kzil88@users.noreply.github.com>
2018-10-13 17:03:55 +0800

    Update Init_50ETF.py

--- Init_50ETF.py
+++ Init_50ETF.py
@@ -5,6 +5,7 @@ import pymysql
 import re
 import talib as ta
 
+
 if __name__ == '__main__':
     db = pymysql.connect(host='127.0.0.1', user='root', passwd='admin', db='stock', charset='utf8')
     cursor = db.cursor()
